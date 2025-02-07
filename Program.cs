57f7d172f998331d4ad6d9c1b86a86d0c19fab83
Mirmahmud03 <mahmudkamilof@gmail.com>
2025-02-07 15:57:21 +0500

    Doiraning yuzi

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿
+Console.Write("doirani radiusini kiriting:");
+double radius = Convert.ToDouble(Console.ReadLine());
+
+double s = 3.14159265358979 * radius * radius;
+double l = 2 * 3.14159265358979 * radius;
+
+Console.WriteLine(" S = " + s);
+Console.WriteLine(" L = " + l);
